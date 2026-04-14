6e25feca6ff6adfba1a17894055221c773b49645
alex <alexbr1208@gmail.com>
2026-04-14 13:02:41 -0400

    Implement player health system and update main function to display total health

--- src/main.cpp
+++ src/main.cpp
@@ -6,17 +6,35 @@
 class Player
 {
 public:
-    int c_health;
-    std::string c_name;
+    // TODO: Finish the health system with multiple limbs.
+    int m_mainHealth;
+    int m_head;
+    int m_leftArm;
+    int m_rightArm;
+    int m_torso;
+    int m_leftLeg;
+    int m_rightLeg;
+    int m_armour;
+
+
+    Player() : m_head(40), m_torso(60), m_leftArm(75), m_leftLeg(75), m_rightArm(75), m_rightLeg(75) 
+    {
+
+    }
+
+    int getTotalHealth() {
+        return(m_head + m_torso + m_leftArm + m_leftLeg + m_rightArm + m_rightLeg);
+
+    }
 };
 
 int main()
 {
-    // TODO: YES!
-    Player PlayerCharacter;
-    PlayerCharacter.c_health = {100};
-    PlayerCharacter.c_name = "Dih";
-    std::cout << PlayerCharacter.c_health << PlayerCharacter.c_name;
+    Player mainPlayer; // creates the player character
+
+    std::cout << mainPlayer.getTotalHealth();
+
+    // TEMP: KEEP WINDOW OPEN
     int hold;
     std::cin >> hold;
     return 0;
